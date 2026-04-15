486110eb3b7d6f20cbc5411ea0c751121132456a
feron29041984-collab <feron29041984@gmail.com>
2026-04-15 20:58:17 +0300

    Update AdBanner.jsx

--- src/components/AdBanner.jsx
+++ src/components/AdBanner.jsx
@@ -1,7 +1,7 @@
 function AdBanner() {
   return (
     <a
-      href="https://www.profitablecpmratenetwork.com/pu6j6zgf?key=4eb4d4eecfc2b1aa96fc7e220776e460"
+      href="https://ferocitycandour.com/pu6j6zgf?key=4eb4d4eecfc2b1aa96fc7e220776e460"
       target="_blank"
       rel="noopener noreferrer sponsored"
       className="ad-banner"
